673c6ddedf3fd0afdde22f77504a055d64f3ebec
itallominatti <itallominatti@gmail.com>
2024-10-03 10:42:28 -0300

    feat main -> começando os cards dos coffees

--- src/components/Coffees/index.tsx
+++ src/components/Coffees/index.tsx
@@ -1,11 +1,18 @@
 import styles from './Coffees.module.css';
+import Image from 'next/image';
 
 export default function Coffees() {
     return (
         <section className={styles.feedCoffee}>
             <h2 className={styles.feedCoffee__title}>Nossos cafés</h2>
             <div className={styles.feedCoffee__wrapperCoffees}>
-                
+                <div className={styles.coffeeCard}>
+                    <Image src="/" alt="" width={120} height={120} /> 
+                    <div className={styles.coffeCard__wrapperPills}>
+                        <span className={styles.pill}>tradicional</span>
+                        <span className={styles.pill}>com leite</span>
+                    </div>
+                </div>
             </div>
         </section>
     )
